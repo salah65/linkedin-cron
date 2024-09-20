8996a44bcfbbd9e5974d6728af12a8b6a871b818
Magdy Edwar <70660386+Magdyedwar1996@users.noreply.github.com>
2024-09-19 19:29:01 -0500

    Adding the script bash file and the requirements files

---
+++ script.sh
@@ -0,0 +1,21 @@
+
+# to install the venv
+python3 -m venv venv
+source venv/bin/activate 
+
+
+# to install the required packages
+
+pip3 install selenium
+pip3 install python-dotenv
+pip3 install pandas
+pip3 install openpyxl
+
+
+# to run this file 
+# bach filename.sh
+
+
+
+
+
